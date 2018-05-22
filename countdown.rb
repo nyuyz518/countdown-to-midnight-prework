c36f8fa8864118e8c4e35c56d003022e99f593ad
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-22 02:13:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
+  def countdown(count)
+    while count >0 do 
+      puts "#{count} seconds!"
+      count-=1
+    end
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
-end
+  def countdown_with_sleep(count)
+    while count > 0 do 
+      count-=1
+      sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+  end
